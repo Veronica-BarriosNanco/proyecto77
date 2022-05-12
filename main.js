9af82b467ef775c9f5485b1f825628a304cce9c8
Veronica-BarriosNanco <100185817+Veronica-BarriosNanco@users.noreply.github.com>
2022-05-11 20:13:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("Mi primer proyecto en la consola");
+
+            var primer_nombre = "Ranbir";
+            var apellido = "Kapoor";
+
+            var nombre_completo = primer_nombre.concat(apellido);
+            console.log(nombre_completo);
+
+            var a = 10;
+            var b = 5;
+
+            var sumar = a + b ;
+            console.log(sumar);
+
+            var restar = a - b ;
+            console.log(restar);
+
+            var multiplicar = a * b ;
+            console.log(multiplicar);
+
+            var dividir = a / b ;
+            console.log(dividir);
